bea110688d8e48d66fcea7645c4b53063592b1af
Tyler Bobik <shed412@gmail.com>
2017-09-30 20:12:38 -0400

    did add_vctors test

--- point.c
+++ point.c
@@ -2,6 +2,7 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void add_vec_to_point(point* p1, vector vec){
 	
@@ -62,3 +63,54 @@ void xy_rotate(double deg, vector* vec_ret){}
 void xz_rotate(double deg, vector* vec_ret){}
 void yz_rotate(double deg, vector* vec_ret){}
 void scale(double x_mult, double y_mult, double z_mult, vector* vec_ret){}
+
+void add_vec_test(){
+	point p1, p2, p3, p4;
+	vector vec1, vec2;
+
+	p1.x = 1;
+	p1.y = 1;
+	p1.z = 1;
+
+	p2.x = 2;
+	p2.y = 2;
+	p2.z = 2;
+
+	vec1.start = p1;
+	vec1.end = p2;
+	vec1.magnitude = get_mag(p1, p2);
+
+	p2.x = 3;
+	p2.y = 3;
+	p2.z = 3;
+
+	p4.x = 4;
+	p4.y = 4;
+	p4.z = 4;
+
+	vec2.start = p3;
+	vec2.end = p4;
+	vec2.magnitude = get_mag(p3, p4);
+
+	add_vectors(&vec1, vec2);
+
+	int sp1x = vec1.start.x;
+	int sp1y = vec1.start.y;
+	int sp1z = vec1.start.z;
+
+	int ep1x = vec1.end.x;
+	int ep1y = vec1.end.y;
+	int ep1z = vec1.end.z;
+
+	if(sp1x==4 && sp1y==4 && sp1z==4) printf("Start Point passes\n");
+	else printf("Start Point fails\n");
+
+	if(ep1x==6 && ep1y==6 && ep1z==6) printf("End Point passes\n");
+	else printf("End Point fails\n");
+}
+
+int main(){
+
+	add_vec_test();
+
+}
